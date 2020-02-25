e6d730d6d327a2b00cebeb2ff79c2627408e1a91
Ben Feole <bfeole@gmail.com>
2020-02-25 15:19:22 -0500

    Linked list setup insertFirst

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,29 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor(){
+        this.head = null;
+    }
+    // Insert First Method
+    insertFirst(data) {
+        // create a new Node, set the next value to this.head pointing the new node to the previous head
+        // If there is no head value it will be set to null
+        this.head = new Node(data, this.head);
+    }
+}
+
+
+
+// Example:
+// const list = new LinkedList();
+// list.head = new Node(10)
 
 module.exports = { Node, LinkedList };
