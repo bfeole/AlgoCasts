7157ef6110f3985c50f5553e41067ad0bab71f48
Ben Feole <bfeole@gmail.com>
2020-02-27 19:09:41 -0500

    Reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,29 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// first pass
+// get reversed string of ints
+// if n == 0, return 0
+// if input is positive convert nums to int and return
+// if input is negative, return negative abs value of converted string int
+// function reverseInt(n) {
+//   let nums = n.toString().split('').reverse().join('');
+//   if(n == 0){
+//       return 0;
+//   } else {
+//       return (Math.sign(n) === 1 ? parseInt(nums) : -Math.abs(parseInt(nums)));
+//   }
+// }
+
+// -- 1
+function reverseInt(n) {
+    let nums = n.toString().split('').reverse().join('');
+    return parseInt(nums) * Math.sign(n);
+
+    // if (n < 0) {
+    //     return parseInt(reversed) * -1;
+    // }
+    // return parseInt(reversed);
+}
 
 module.exports = reverseInt;
