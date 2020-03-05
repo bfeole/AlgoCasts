5d5a9682209f0fe47df617413d319c82a9b4a950
Ben Feole <bfeole@gmail.com>
2020-03-05 17:28:51 -0500

    Capitalize first char in str for loop and slice

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,37 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// First Pass
+
+// function capitalize(str) {
+//     // create an empty holding array
+//     const words = [];
+//     // loop through given string and split by space
+//     for ( let word of str.split(' ')) {
+//         // uppercase first character of each word and add to word - index 1
+//         words.push(word[0].toUpperCase() + word.slice(1));
+//     }
+//     // rejoin separated words by spaces and return str with uppercased first letter
+//     return words.join(' ');
+// }
+
+function capitalize(str) {
+    // create 'result' which is the first char of the input string capitalized
+    let result = str[0].toUpperCase();
+    // for each char in strin
+    for (let i = 1; i < str.length; i++) {
+        // if the character to the left a space
+        if (str[i - 1] === ' ') {
+            // capitalize it and add it to result
+            result+= str[i].toUpperCase();
+        } else {
+            // else add it to result
+            result += str[i];
+        }
+    }
+
+    return result;
+
+}
 
 module.exports = capitalize;
